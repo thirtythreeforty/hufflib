b32f77824c980563df95e9ef7b6043b2d806baed
George Hilliard <thirtythreeforty@lavabit.com>
2013-06-26 21:54:33 -0500

    Implement unhuff
    Also implemented setUpOffsets, which is needed by unhuff.

--- hufflib.asm
+++ hufflib.asm
@@ -24,11 +24,28 @@ libId .equ 0x60
 .org 0
 
 jumpTable:
-    ret \ nop \ nop
+    jp setUpOffsets
     ret \ nop \ nop
     jp huff
     jp unhuff
     .db 0xFF
+
+setUpOffsets:
+	push ix
+	push hl
+	push de
+	icall(_)
+_:	pop ix
+	call memSeekToStart
+	push ix \ pop de
+	ild(hl, offsetNeeded2)
+	ld (hl), e
+	inc hl
+	ld (hl), d
+	pop de
+	pop hl
+	pop ix
+	ret
 
 ;; huff
 ;;  Compresses data using Huffman coding.
@@ -52,6 +69,72 @@ huff:
 ;;  HL: Data to decompress
 ;;  DE: Location for uncompressed data
 unhuff:
+	push af
+	push hl
+	push bc
+	push ix
+	push de
+	push de
+		; Set up state
+		ld c, (hl)			; Uncompressed data size, into BC
+		inc hl
+		ld b, (hl)
+		inc hl
+		inc hl				; (skip) Huffman tree to use, not yet implemented
+		ld d, (hl)			; First byte of data into D, TODO jump to correct location or something
+		inc hl
+		ld e, 0xFF			; Mask into E
+.reroot:
+		ild(ix, tree)		; Root of tree into IX
+
+		; Check to ensure bytes are left
+.nextBit:
+		ld a, b \ or c \ jr z, .done
+
+		; Get a bit
+		sla d
+		push af
+			srl e
+			jr nz, _
+			ld d, (hl)
+			inc hl
+			ld e, 0xFF
+_:		pop af
+
+		; Shift to the correct location in the node
+		jr nc, _
+		inc ix
+		inc ix
+_:		push hl
+			ld l, (ix + 3)
+			ld h, (ix + 4)
+			push de
+offsetNeeded2 .equ $ + 1
+				ld de, 0x0000
+				add hl, de
+			pop de
+			ex (sp), hl
+		pop ix
+		; Determine if we've reached a leaf
+		bit 0, (ix)
+		jr z, .nextBit
+
+		; Leaf!  Handle it.
+		ld a, (ix + 3)
+		ex (sp), hl
+		ld (hl), a
+		inc hl
+		ex (sp), hl
+		dec bc
+		jr .reroot
+
+.done:
+	pop de
+	pop de
+	pop ix
+	pop bc
+	pop hl
+	pop af
 	ret
 
 #include "hufftree.asm"
